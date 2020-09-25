12a49fb13e50383eacfd616f0f03d75a3ff364ce
anisma <anismaghfiroh63@gmail.com>
2020-09-25 15:42:00 +0700

    fixing responsive

--- resources/js/header.js
+++ resources/js/header.js
@@ -5,28 +5,27 @@ let scroll_position = window.scrollY;
 let ticking = false;
 
 function addClass(scroll_pos) {
-    header.classList.add('scroll-header')
+   header.classList.add('scroll-header');
 }
 
 function removeClass(scroll_pos) {
-    header.classList.remove('scroll-header')
+   header.classList.remove('scroll-header');
 }
 
 if (scroll_position != 0) {
-    addClass(scroll_position);
+   addClass(scroll_position);
 }
 
 window.addEventListener('scroll', function(e) {
-    scroll_position = window.scrollY;
+   scroll_position = window.scrollY;
 
-    if (scroll_position != 0) {
-        window.requestAnimationFrame(function() {
-            addClass(scroll_position);
-        });
-    } else {
-        window.requestAnimationFrame(function() {
-            removeClass(scroll_position);
-        });
-    }
-
+   if (scroll_position != 0) {
+      window.requestAnimationFrame(function() {
+         addClass(scroll_position);
+      });
+   } else {
+      window.requestAnimationFrame(function() {
+         removeClass(scroll_position);
+      });
+   }
 });
